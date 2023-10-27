67af730597e9d154b18b26488cbcc84f8f917f8f
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-10-27 14:34:54 -0600

    custom http and avoiding common errors

---
+++ src/hooks/useHttp.js
@@ -0,0 +1,47 @@
+import { useCallback, useState, useEffect } from "react";
+
+async function sendHttpRequest(url, config) {
+	const response = await fetch(url, config);
+	const resData = await response.json();
+
+	if (!response.ok) {
+		throw new Error(
+			resData.message || "SOMETHING WENT WRONG, FAILED TO SEND REQUEST.",
+		);
+	}
+	return resData;
+}
+
+export default function useHttp(url, config, initialData) {
+	const [data, setData] = useState(initialData);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState();
+
+	const sendRequest = useCallback(
+		async function sendRequest() {
+			setIsLoading(true);
+
+			try {
+				const resData = await sendHttpRequest(url, config);
+				setData(resData);
+			} catch (error) {
+				setError(error.message || "SOMETHING WENT WRONG");
+			}
+			setIsLoading(false);
+		},
+		[url, config],
+	);
+
+	useEffect(() => {
+		if (!config || config.method === "GET" || !config.method || !config) {
+			sendRequest();
+		}
+	}, [sendRequest, config, url]);
+
+	return {
+		data,
+		isLoading,
+		error,
+		sendRequest,
+	};
+}
